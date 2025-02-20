000524ba3d1fe511dca5a98a34103af0e96ac559
Maksim Alipau <maxanotest@gmail.com>
2025-02-20 15:09:01 +0300

    feat: add a task 2

---
+++ hw/task2.js
@@ -0,0 +1,7 @@
+// Добавление и удаление свойств
+
+// Добавьте в объект user новое свойство isStudent со значением true, затем удалите свойство city.
+user.isStudent = true;
+delete user.city;
+
+console.log(user);
