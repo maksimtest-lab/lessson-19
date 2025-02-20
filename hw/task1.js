71ac69cca9c91742138355c2b0d2367e223eb810
Maksim Alipau <maxanotest@gmail.com>
2025-02-20 15:04:58 +0300

    feat: add a task 1

---
+++ hw/task1.js
@@ -0,0 +1,11 @@
+// Создание объекта
+
+// Создайте объект user с полями name, age, city. Выведите его в консоль.
+
+const user = {
+    name: 'John',
+    age: 21,
+    city: 'Moscow'
+};
+
+console.log(user);
